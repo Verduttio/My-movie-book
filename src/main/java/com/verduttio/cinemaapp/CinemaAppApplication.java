814e0bfd78d9df2224eb52800de1d71478ba9576
verduttio <bszwaja20@gmail.com>
2022-09-02 12:16:09 +0200

    File handling

--- src/main/java/com/verduttio/cinemaapp/CinemaAppApplication.java
+++ src/main/java/com/verduttio/cinemaapp/CinemaAppApplication.java
@@ -1,13 +1,27 @@
 package com.verduttio.cinemaapp;
 
+import com.verduttio.cinemaapp.entity.storage.StorageProperties;
+import com.verduttio.cinemaapp.service.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class CinemaAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CinemaAppApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
     }
 
 }
---
+++ src/main/java/com/verduttio/cinemaapp/entity/storage/StorageException.java
@@ -0,0 +1,12 @@
+package com.verduttio.cinemaapp.entity.storage;
+
+public class StorageException extends RuntimeException {
+
+    public StorageException(String message) {
+        super(message);
+    }
+
+    public StorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
