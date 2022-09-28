0d3f93752be7a72b3f42e94faeb9ec21a809cba9
verduttio <bszwaja20@gmail.com>
2022-09-28 20:52:05 +0200

    Changed file name generator from names using date to names generated by uuid

--- Cinema-app-backend/src/main/java/com/verduttio/cinemaapp/service/storage/FileNameGenerator.java
+++ Cinema-app-backend/src/main/java/com/verduttio/cinemaapp/service/storage/FileNameGenerator.java
@@ -1,14 +1,9 @@
 package com.verduttio.cinemaapp.service.storage;
 
-import java.time.LocalDateTime;
+import java.util.UUID;
 
 public class FileNameGenerator {
     public static String generateName() {
-        LocalDateTime localDateTime = LocalDateTime.now();
-        String name = localDateTime.toString();
-        name = name.replace(':', '_');
-        name = name.replace('-', '_');
-        name = name.replace('.', '_');
-        return name;
+        return UUID.randomUUID().toString();
     }
 }
